fd567322de3bf48045e4578dc5ef8a928b46c7e4
JoeMC0y <80136727+JoeMC0y@users.noreply.github.com>
2022-12-08 16:35:40 -0700

    json reader done

--- stock_statements/src/main/java/edu/neumont/csc180/winters/dylan/controller/jsonreader.java
+++ stock_statements/src/main/java/edu/neumont/csc180/winters/dylan/controller/jsonreader.java
@@ -2,6 +2,7 @@ package edu.neumont.csc180.winters.dylan.controller;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.json.simple.parser.*;
@@ -12,11 +13,11 @@ import org.json.simple.parser.JSONParser;
 
 import edu.neumont.csc180.winters.dylan.object.Person;
 import edu.neumont.csc180.winters.dylan.object.Trades;
+import edu.neumont.csc180.winters.dylan.object.person;
 
 public class jsonreader{
 
     private static JSONArray data;
-
     public static void readJSON(String path) {
         JSONParser parser = new JSONParser();
 
@@ -28,9 +29,49 @@ public class jsonreader{
 
     }
 
-    public static List<Person> getPersonsFromData() {
-        List
+    private static Person makePerson(JSONObject p) {
+        Person person = new Person();
+        person.setAccNum((int) p.get("account_number"));
+        person.setSsn((String) p.get("ssn"));
+        person.setFName((String) p.get("first_name"));
+        person.setLName((String) p.get("last_name"));
+        person.setEmail((String) p.get("email"));
+        person.setPhone((String) p.get("phone"));
+        person.setBegin((double) p.get("beginning_balance"));
+        person.setTrades(getTrades((JSONArray) p.get("stock_trades")));
+        return person;
     }
+
+    private static List<Trades> getTrades(JSONArray d) {
+    List<Trades> trades = new ArrayList<>();
+
+        for (Object t : d){
+            JSONObject trade = (JSONObject) t;
+            Trades tradee = new Trades();
+            tradee.setBuySell((String) trade.get("type"));
+            tradee.setSharePrice((double) trade.get("price_per_share"));
+            tradee.setShares((int) trade.get("count_shares"));
+            tradee.setSymbol((String) trade.get("stock_symbol"));
+
+            trades.add(tradee);
+        }
+
+        return trades;
+
+    }
+
+    public static List<Person> getPeopleFromData() {
+        List<Person> people = new ArrayList<>();
+
+        for (Object o : data){
+            Person person = makePerson((JSONObject) o);
+            people.add(person);
+        }
+
+        return people;
+    }
+
+
 
 
 }
